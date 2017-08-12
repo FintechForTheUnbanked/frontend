52b5960d8d5685f0543ad8fe823dbbd3d64d9abd
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2017-08-12 17:31:18 +0200

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <router-link to="home">Home</router-link>
     <router-link to="settings">Settings</router-link>
     <router-view></router-view>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-	 <ul>
-	 	<li><a class="button is-success">Bulma is active</a></li>
-	 </ul>
+    {{$route.path}}
+    <div id="mainNav" class="tabs">
+      <ul>
+        <li :class="{'is-active': $route.path == '/Home'}">
+          <router-link to="Home">Overview</router-link>
+        </li>
+        <li :class="{'is-active': $route.path == '/Groups'}">
+          <router-link to="Groups">Groups</router-link>
+        </li>
+        <li :class="{'is-active': $route.path == '/Settings'}">
+          <router-link to="Settings">Settings</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -40,30 +35,9 @@ export default {
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#mainNav{
+  position: absolute;
+  bottom: 0;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
